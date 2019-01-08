7cf08ab5b6a39e5564abb6aad4df91ceb093d6f4
Saif Ijaz <41892175+saif86@users.noreply.github.com>
2019-01-08 14:15:12 +0800

    Update Task1_main.cpp

--- Task1_main.cpp
+++ Task1_main.cpp
@@ -1,18 +1,32 @@
-#include<iostream>
-#include<iomanip> 
-#include "Time.h"
+/**
+* @file Task1_main.cpp
+*
+* @brief This code implements creates array of Time objects
+* and add minutes seperately.
+*
+* @author Saif Ullah Ijaz
+*
+*/
 
+// SYSTEM INCLUDES
+#include <iostream> 
 using namespace std;
 
+// LOCAL INCLUDES
+#include "time.h"
 
+
+
+// function main begins program execution
 void main() {
-	Time::setDefaultTime(15, 45, 0);
-	Time t[4] = { Time(12,30),Time(14,50),Time(24,12) };
+
+	Time::sSetDefaultTime(15, 45, 0);
+	Time t[4] = { Time(12,30),Time(14,50),Time(23,59) };
 
 	t[0].Display24();
 	t[0].Display12();
 	cout << "Adding 15 minutes" << endl;
-	t[0].addMinute(15);
+	t[0].AddMinute(15);
 	t[0].Display24();
 	t[0].Display12();
 	cout << endl;
@@ -20,7 +34,7 @@ void main() {
 	t[1].Display24();
 	t[1].Display12();
 	cout << "Adding 20 minutes" << endl;
-	t[1].addMinute(20);
+	t[1].AddMinute(20);
 	t[1].Display24();
 	t[1].Display12();
 	cout << endl;
@@ -28,13 +42,15 @@ void main() {
 	t[2].Display24();
 	t[2].Display12();
 	cout << "Adding 2 minutes" << endl;
-	t[2].addMinute(2);
+	t[2].AddMinute(2);
 	t[2].Display24();
 	t[2].Display12();
 	cout << endl;
-	
+
 	t[3].Display24();
 	t[3].Display12();
 
+
 	system("pause");
 }
+// end main
